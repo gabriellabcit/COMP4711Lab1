8c8441c0ecd5e1d81cdd9eb74e8af8b1ac8679e0
Gabriella Cheung <gabriella.bcit@gmail.com>
2016-01-12 17:24:59 -0800

    Finished PHP Iteration step

--- index.php
+++ index.php
@@ -22,41 +22,15 @@
         }
         
         function winner($token,$position) {
-            $won = false;
-            if (($position[0] == $token) &&
-                    ($position[1] == $token) &&
-                    ($position[2] == $token)) {
-                $won = true;
-            } else if (($position[3] == $token) &&
-                    ($position[4] == $token) &&
-                    ($position[5] == $token)) {
-                $won = true;
-            } else if (($position[6] == $token) &&
-                    ($position[7] == $token) &&
-                    ($position[8] == $token)) {
-                $won = true;
-            } else if (($position[0] == $token) &&
-                    ($position[3] == $token) &&
-                    ($position[6] == $token)) {
-                $won = true;
-            } else if (($position[1] == $token) &&
-                    ($position[4] == $token) &&
-                    ($position[7] == $token)) {
-                $won = true;
-            } else if (($position[2] == $token) &&
-                    ($position[5] == $token) &&
-                    ($position[8] == $token)) {
-                $won = true;
-            } else if (($position[0] == $token) &&
-                    ($position[4] == $token) &&
-                    ($position[8] == $token)) {
-                $won = true;
-            } else if (($position[2] == $token) &&
-                    ($position[4] == $token) &&
-                    ($position[6] == $token)) {
-                $won = true;
+            for($row=0; $row<3; $row++) {
+                $result = true;
+                for ($col=0; $col<3; $col++) {
+                    if($position[3*$row+$col] != token) {
+                        $result = false;
+                    }
+                }
             }
-            return $won;
+            return $result;
         }
         ?>
     </body>
